73a9cb6645edb8a91eda040c9baee5601e371733
unknown <oluwatomiwaakinbode@gmail.com>
2022-05-23 19:06:30 +0100

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import work from './images/work.svg';
-import axios from 'axios';
+// import axios from 'axios';
 
 function App() {
   // const [shortly, setShortly] = useState({});
